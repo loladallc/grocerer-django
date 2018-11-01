d9b55436341c4dad2f2865bfa025c940d864f97a
Siddharth Gangadhar <siddharth.gangadhar@gmail.com>
2018-11-01 17:20:46 -0400

    understand how request.POST vs. request.body works

--- owner/views.py
+++ owner/views.py
@@ -5,15 +5,22 @@ from django.http import HttpResponse
 # Create your views here.
 from pprint import pprint
 from django.views.decorators.csrf import csrf_exempt
-
+from django.views.decorators.http import require_http_methods
+import json
 
 def index(request):
     return HttpResponse("This is the owner page")
 
 
 @csrf_exempt
+@require_http_methods(["POST"])
 def register_store(request):
     if request.method == "POST":
-        params = request.POST
-        pprint(params)
+        if request.content_type == "application/x-www-form-urlencoded":
+            params = request.POST
+            print(params)
+            print(params.get("username"))
+        else:
+            params = request.body
+            print(json.loads(params)["username"])
         return HttpResponse(params)
